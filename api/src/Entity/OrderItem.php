40c351275cb4aa966879ac7b7582a8a49827e7f6
Ruben van der Linde <ruben@conduction.nl>
2019-12-16 08:38:35 +0100

    Add @maxdepth usage

--- api/src/Entity/OrderItem.php
+++ api/src/Entity/OrderItem.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiProperty;
-use DateTime;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
