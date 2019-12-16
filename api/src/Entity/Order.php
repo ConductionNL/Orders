fac0289733dd4af166660c933abed535f54439eb
Ruben van der Linde <ruben@conduction.nl>
2019-12-16 06:26:40 +0100

    ORM fix

--- api/src/Entity/Order.php
+++ api/src/Entity/Order.php
@@ -32,7 +32,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true}
  * )
  * @ORM\Entity(repositoryClass="App\Repository\OrderRepository")
- * @Table(name="orders")
+ * @ORM\Table(name="orders")
  */
 class Order
 {
